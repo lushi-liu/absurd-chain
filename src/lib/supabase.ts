4f1bafeec13210996e7d58cec351825f58757e47
Lushi Liu <lushiliu1@gmail.com>
2026-02-03 19:09:53 +0900

    fixed error with supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL ?? 'https://test.supabase.co';
+const supabaseKey = process.env.SUPABASE_ANON_KEY ?? 'testkey';
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
